4467cb05df8fe6d140b5309286c72ea1e0cea566
MuhammadUmerKhan <muhammadumerk546@gmail.com>
2024-01-04 17:44:01 +0500

    machine learning files

--- All Files/machinelearning4.py
+++ All Files/machinelearning4.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 import numpy as np 
 import pandas as pd
@@ -22,3 +25,28 @@ le_Chol.fit(['NORMAL', 'HIGH'])
 x[:,3] = le_Chol.transform(x[:,3])
 
 y = df["Drug"]
+# Setting up Decision Tree
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=3)
+x_train.shape
+y_train.shape
+
+print("Shape of xTrain set is {}".format(x_train.shape), "and", "Size of xTrain set is {}".format(y_train.shape))
+
+x_test.shape
+y_test.shape
+
+# Modeling
+drugTree = DecisionTreeClassifier(criterion='entropy', max_depth=4)
+drugTree.fit(x_train, y_train)
+
+# Prediction 
+predTree = drugTree.predict(x_test)
+predTree[0:5]
+y_test[0:5]
+
+# Evaluation
+print("DecisionTree's Accuracy: {}".format(metrics.accuracy_score(y_test, predTree)))
+
+# Visualization
+tree.plot_tree(drugTree)
+plt.show()
