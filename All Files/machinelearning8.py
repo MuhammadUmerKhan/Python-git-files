3521c1d6f304cd055cdfbfc016a090ca0a52f1e3
MuhammadUmerKhan <muhammadumerk546@gmail.com>
2024-01-14 09:58:04 +0500

    Update machinelearning8.py

--- All Files/machinelearning8.py
+++ All Files/machinelearning8.py
@@ -1,6 +1,10 @@
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report, confusion_matrix
+import itertools
 from sklearn import svm
 from cProfile import label
-from turtle import color
+from turtle import color, pos
 import pandas as pd
 import pylab as pl
 import numpy as np
@@ -58,3 +62,64 @@ clf = svm.SVC(kernel='rbf')
 clf.fit(x_train, y_train)
 yhat = clf.predict(x_test)
 yhat[0:5]
+
+# Evaluation
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, yhat, labels=[2,4])
+np.set_printoptions(precision=2)
+
+print (classification_report(y_test, yhat))
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['Benign(2)','Malignant(4)'],normalize= False,  title='Confusion matrix')
+
+f1_score(y_test, yhat, average='weighted')
+
+jaccard_score(y_test, yhat, pos_label=2)
+
+# Practice
+# Can you rebuild the model, but this time with a __linear__ kernel? 
+# You can use __kernel='linear'__ option, when you define the svm. 
+# How the accuracy changes with the new kernel function?
+clf1 = svm.SVC(kernel='linear')
+clf1.fit(x_train, y_train)
+yhat1 = clf1.predict(x_test)
+yhat1[0:5]
+yhat[0:5]
+print("Average f1-score is : %.4f"% f1_score(y_test, yhat1, average='weighted'))
+print("Jaccord SCore is: %.4f"% jaccard_score(y_test, yhat1, pos_label=2))
