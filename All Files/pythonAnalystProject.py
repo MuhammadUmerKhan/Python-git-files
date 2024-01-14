15c4c89630ebb1f9493bae4d3085a87b6ad75d97
MuhammadUmerKhan <muhammadumerk546@gmail.com>
2024-01-14 11:24:10 +0500

    Analyst Project

--- All Files/pythonAnalystProject.py
+++ All Files/pythonAnalystProject.py
@@ -1,3 +1,5 @@
+import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 import matplotlib
@@ -53,5 +55,67 @@ df['runtime'] = df['runtime'].astype('int64')
 df['votes'] = df['votes'].astype('int64')
 df.dtypes
 
-df.sort_values(by=['gross'], inplace=False, ascending=False)
+df = df.sort_values(by=['gross'], inplace=False, ascending=False)
 pd.set_option('display.max_rows', None)
+
+df.to_csv('movies.csv')
+df = pd.read_csv('movies.csv')
+df.drop(columns='Unnamed: 0', inplace=True)
+df.head()
+
+# Drop any duplicated
+df['company'] = df['company'].drop_duplicates().sort_values(ascending=False)
+
+# Budget high correlation
+# company high correlation
+# scatter plot with budget with gross
+df.columns
+fig = px.scatter(df, x='budget', y='gross', title='Budget vs Gross Earning', size_max=20,)
+fig.update_layout(xaxis_title='Gross Earning', yaxis_title='Budget for Film')
+fig.show()
+
+# plot using seaborn
+plt.figure(figsize=(10, 6))
+sns.regplot(x='budget', y='gross', data=df, scatter_kws={'color':'red'}, line_kws={'color':'blue'})
+plt.xlabel('Gross Earning')
+plt.ylabel('Budget for Film')
+plt.title('Budget vs Gross Earning')
+plt.show()
+
+# For Actual correslation
+correlation = df[['budget', 'gross', 'runtime', 'score', 'votes', 'year']]
+correlation.corr() # mwthod = pearson, kendall, spearman
+# High correlation between bidget and gross is high
+
+correlation_matriix = correlation.corr(method='pearson')
+plt.figure(figsize=(10, 6))
+sns.heatmap(correlation_matriix, annot=True)
+plt.xlabel('Movie Features')
+plt.ylabel('Movie Features')
+plt.title('Correlation Matric For Numeric Features')
+plt.show()
+
+df_numerized = df
+for col_name in df_numerized.columns:
+    if (df_numerized[col_name].dtype == 'object'):
+        df_numerized[col_name] = df_numerized[col_name].astype('category')
+        df_numerized[col_name] = df_numerized[col_name].cat.codes
+        
+df_numerized.head()
+
+
+correlation_matrix = df_numerized.corr(method='pearson')
+plt.figure(figsize=(20, 10))
+sns.heatmap(correlation_matrix, annot=True)
+plt.xlabel('Movie Features')
+plt.ylabel('Movie Features')
+plt.title('Correlation Matric For Numeric Features')
+plt.show()
+
+
+correlation_matrix = df_numerized.corr(method='pearson')
+corr_pairs = correlation_matrix.unstack()
+sorted_pairs = corr_pairs.sort_values()
+
+high_corr = sorted_pairs[(sorted_pairs) > 0.5]
+high_corr
