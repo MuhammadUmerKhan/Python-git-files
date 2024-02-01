6fa5a825b3d4dc1525a6e59cb6f535e4a304f2a9
MuhammadUmerKhan <muhammadumerk546@gmail.com>
2024-02-01 16:50:51 +0500

    EDA

--- All Files/cities_air_quality_water_polu_EDA.py
+++ All Files/cities_air_quality_water_polu_EDA.py
@@ -1,6 +1,7 @@
 import folium as fp
 import branca
 import branca.colormap as cm
+import folium
 import seaborn as sns
 from turtle import color
 import pandas as pd
@@ -11,19 +12,12 @@ from plotly import express as px
 from matplotlib import legend
 from folium import plugins
 
+# Un comment and run this bolck of code
 
 url = 'https://raw.githubusercontent.com/VicmanGT/KaggleCompetition/b39212ebd9c8106a00d7e9823183c6e7cb9305c6/Data/cities_air_quality_water_pollution.18-10-2021%20(1)%20with_coordiantes.csv'
-df = pd.read_csv(url)
-df.head()
-df.drop(columns="Unnamed: 0", inplace=True)
-df.shape
-df.isnull().sum()
-
-df.describe()
-df.info()
-
-df.duplicated().any()
-# Un comment and run this bolck of code
+# df = pd.read_csv(url)
+# df.head()
+# df.drop(columns="Unnamed: 0", inplace=True)
 
 # df = df.applymap(lambda x: x.replace('"', '') if isinstance(x, str) else x)
 # df = df.rename(columns=lambda x: x.replace('"', ''))
@@ -31,8 +25,13 @@ df.duplicated().any()
 
 df = pd.read_csv("cities_air_quality_water_pollution.csv")
 df.head()
+df.shape
+df.isnull().sum()
 
+df.describe()
+df.info()
 
+df.duplicated().any()
 
 # We Found no any outlier and no any missing values and its time to visualize it
 df.head()
@@ -173,5 +172,93 @@ plt.show()
 
 # Geovisualization
 df.head()
-map = fp.Map()
-map = fp.Map(locations=[40.712728, -74.006015], zoom_start=4)
+Map = fp.Map()
+Map = fp.Map(locations=[40.712728, -74.006015], zoom_start=8)
+# print(Map)
+
+# Air Quality
+# Plotting for city
+water_cm = cm.LinearColormap(["#80FFFF", "#000000"], vmin=0, vmax=100, caption='Water Pollution').to_step(5)
+air_cm = cm.LinearColormap(["#000000", "#D6D6D6"], vmin=0, vmax=100, caption='Air Quality').to_step(5)
+Lat = list(df['Lat'])
+Lon = list(df['Lon'])
+air = list(df[' AirQuality'])
+water = list(df[' WaterPollution'])
+
+air_world_map = fp.Map(location=[0, 0], zoom_start=4) 
+
+for loc, a in zip(zip(Lat, Lon), air):
+    fp.Circle(
+        location=loc,
+        radius = 5000,
+        fill = True,
+        color = air_cm(a),
+        popup=f'Air Quality {a}',
+        weight = 5        
+    ).add_to(air_world_map)
+air_world_map.add_child(air_cm)
+
+# Water Quality
+water_world_map = fp.Map(location=[0, 0], zoom_start=4)
+for loc, w in zip(zip(Lat, Lon), water):
+    fp.Circle(
+        location=loc,
+        radius = 5000,
+        fill = True,
+        color = water_cm(w),
+        popup=f'Water Polution {w}',
+        weight = 5        
+    ).add_to(water_world_map)
+water_world_map.add_child(water_cm)
+
+# Plotting for country or Choropleths
+df_country = df.groupby(' Country')[[' AirQuality', ' WaterPollution']].mean()
+df_country
+#Removes the doble quotation marks from the country's name and puts the result in a different coulumn
+def quotation(row):
+    return row.replace(' "', '').replace('"', '')
+df_country['Country'] = sorted(df[' Country'].apply(quotation).unique())
+df_country.reset_index(inplace=True)
+df_country.drop(' Country', inplace=True, axis=1)
+df_country
+
+df_country.loc[119, 'Country'] = 'China'
+
+#json file with the contours of each country
+url = ( "https://raw.githubusercontent.com/python-visualization/folium/main/examples/data")
+country_geo = f"{url}/world-countries.json"
+
+air_dict = df_country.set_index('Country')[' AirQuality']
+water_dict = df_country.set_index('Country')[' WaterPollution']
+
+# Air Quality:
+air_choropleth = fp.Map(location=[0, 0], zoom_start=2)
+fp.GeoJson(
+    data=country_geo, 
+    style_function= lambda feature: { 
+        'fillColor': air_cm(air_dict[feature['properties']['name']])
+        if feature['properties']['name'] in air_dict.index
+        else 'white',
+        'color' : 'black', 
+        'fillOpacity': 0.6
+    }, 
+    
+).add_to(air_choropleth)
+air_cm.add_to(air_choropleth)
+air_choropleth
+
+# Water Polution
+water_choropleth = fp.Map(location=[0, 0], zoom_start=2)
+fp.GeoJson(
+    data=country_geo, 
+    style_function= lambda feature: { 
+        'fillColor': water_cm(water_dict[feature['properties']['name']])
+        if feature['properties']['name'] in water_dict.index
+        else 'white',
+        'color' : 'black', 
+        'fillOpacity': 0.6
+    }, 
+    
+).add_to(water_choropleth)
+water_cm.add_to(water_choropleth)
+water_choropleth
---
+++ All Files/faceBookData__EDA.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:\DATA SCIENCE\Python-git-files\dataset\pseudo_facebook.csv\pseudo_facebook.csv")
+df.head()
