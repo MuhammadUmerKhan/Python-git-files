1435c17bede771ed396f4a8b48d31aa93b4c02ba
MuhammadUmerKhan <muhammadumerk546@gmail.com>
2024-02-08 11:31:39 +0500

    ML

--- All Files/ML/PCA_heartCSV.py
+++ All Files/ML/PCA_heartCSV.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import KFold, cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.decomposition import PCA
@@ -79,3 +80,76 @@ df.info()
 df.dtypes
 df.columns
 df.head()
+
+le = LabelEncoder()
+df.head()
+
+df['RestingECG'] = le.fit_transform(df['RestingECG'])
+df.head()
+
+df['ST_Slope'] = le.fit_transform(df['ST_Slope'])
+df.head()
+df['ExerciseAngina'] = le.fit_transform(df['ExerciseAngina'])
+df2 = pd.get_dummies(df, drop_first=True)
+df2.head()
+df2[['ChestPainType_ATA', 'ChestPainType_NAP', 'ChestPainType_TA', 'Sex_M']].dtypes
+df2[['ChestPainType_ATA', 'ChestPainType_NAP', 'ChestPainType_TA', 'Sex_M']] = df2[['ChestPainType_ATA', 'ChestPainType_NAP', 'ChestPainType_TA', 'Sex_M']].astype('int')
+df2.head()
+
+X = df2.drop(columns='HeartDisease', axis='columns')
+Y = df2.HeartDisease
+
+X.head()
+
+X_scaled = StandardScaler().fit_transform(X)
+
+x_train, x_test, y_train, y_test = train_test_split(X_scaled, Y, test_size=0.2, random_state=4)
+
+# logistic regression
+logistic = LogisticRegression(solver='liblinear')
+logistic.fit(x_train, y_train)
+logistic.score(x_test, y_test) # 0.88888888888888888
+
+random = RandomForestClassifier(n_estimators=40)
+random.fit(x_train, y_train)
+random.score(x_test, y_test) # 0.894444444444
+
+# SVM
+svc = SVC()
+svc.fit(x_train, y_train)
+svc.score(x_test, y_test) # 0.894444444
+
+pca = PCA(n_components=10)
+X_pca = pca.fit_transform(X)
+X_train_pca, X_test_pca, y_train, y_test = train_test_split(X_pca, Y, test_size=0.2, random_state=4)
+
+logistic = LogisticRegression(solver='liblinear')
+logistic.fit(X_train_pca, y_train)
+logistic.score(X_test_pca, y_test) # 0.9055555555
+
+# Random Forest
+random = RandomForestClassifier(n_estimators=40)
+random.fit(X_train_pca, y_train)
+random.score(X_test_pca, y_test) # 0.894444444444
+
+# SVM
+svc = SVC()
+svc.fit(X_train_pca, y_train)
+svc.score(X_test_pca, y_test) # 0.75
+
+# cross val score
+log = cross_val_score(LogisticRegression(solver='liblinear', multi_class='ovr'), X_pca, Y, cv=10)
+np.average(log) # 0.829775
+log1 = cross_val_score(LogisticRegression(solver='liblinear', multi_class='ovr'), X, Y, cv=10)
+np.average(log1) # 0.8353183
+
+random1 = cross_val_score(RandomForestClassifier(n_estimators=50), X, Y, cv=10)
+np.average(random1) # 0.84196
+random2 = cross_val_score(RandomForestClassifier(n_estimators=50), X_pca, Y, cv=10)
+np.average(random2) # 0.82079
+
+svc1 = cross_val_score(SVC(gamma='auto'), X, Y, cv=10)
+np.average(svc1) # 0.565
+
+svc2 = cross_val_score(SVC(gamma='auto'), X_pca, Y, cv=10)
+np.average(svc2) # 0.5539
