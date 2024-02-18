488639d5c18345d5f8503485e5347ce24d32a5a1
MuhammadUmerKhan <muhammadumerk546@gmail.com>
2024-02-18 10:42:51 +0500

    ML Practice

---
+++ All Files/ML/Predictive Maintenance/maintenance.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+df = pd.read_csv("/All Files/ML/predictive_maintenance.csv")
